28cd0f793255542f830ba06c5071dcfe106b87b0
Arsh Ahmad <123231460+Ahmad-Arsh@users.noreply.github.com>
2023-04-06 12:50:24 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,19 +1,11 @@
-//your JS code here. If required.
-// Get all the square elements
-const squares = document.querySelectorAll(".square");
 
-// Loop through each square
-squares.forEach((square) => {
-  // Add event listener for mouseover
-  square.addEventListener("mouseover", () => {
-    // Change the background color
-    square.style.backgroundColor = "red";
-  });
-
-  // Add event listener for mouseout
-  square.addEventListener("mouseout", () => {
-    // Set the background color back to its original color smoothly over 1 second
-    square.style.transition = "background-color 1s ease";
-    square.style.backgroundColor = "";
-  });
-});
+for (let i = 0; i < 800; i++) {
+                let square = document.createElement('div');
+                square.classList.add('square');
+                square.addEventListener('mouseover', () => {
+                    setTimeout(() => {
+                        square.style.backgroundColor = '';
+                    }, 1000);
+                });
+                document.querySelector('.container').appendChild(square);
+            }
