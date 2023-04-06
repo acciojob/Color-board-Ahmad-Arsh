f6e239fc907b8c70ba8d9fde12eb40d35c799d9d
Arsh Ahmad <123231460+Ahmad-Arsh@users.noreply.github.com>
2023-04-06 13:06:37 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,17 @@
+  function getRandomColor() {
+                let letters = '0123456789ABCDEF';
+                let color = '#';
+                for (let i = 0; i < 6; i++) {
+                    color += letters[Math.floor(Math.random() * 16)];
+                }
+                return color;
+            }
 
-for (let i = 0; i < 800; i++) {
+            for (let i = 0; i < 800; i++) {
                 let square = document.createElement('div');
                 square.classList.add('square');
                 square.addEventListener('mouseover', () => {
+                    square.style.backgroundColor = getRandomColor();
                     setTimeout(() => {
                         square.style.backgroundColor = '';
                     }, 1000);
